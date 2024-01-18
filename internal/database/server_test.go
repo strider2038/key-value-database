388e18ca1d25f17493188fb5837baa9765c28c76
Igor Lazarev <strider2038@yandex.ru>
2024-01-18 19:54:53 +0300

    homework 03 * add functional test

--- internal/database/server_test.go
+++ internal/database/server_test.go
@@ -5,13 +5,17 @@ import (
 	"testing"
 	"time"
 
+	"github.com/spf13/afero"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/strider2038/key-value-database/internal/config"
+	"github.com/strider2038/key-value-database/internal/database"
 	"github.com/strider2038/key-value-database/internal/database/network"
 	"github.com/strider2038/key-value-database/internal/di"
 	"go.uber.org/goleak"
 )
+
+const ServerAddress = ":11000"
 
 func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m)
@@ -22,7 +26,7 @@ type ServerTestStep struct {
 	WantResponse string
 }
 
-func TestServer_Serve(t *testing.T) {
+func TestServer_Serve_NonPersistentMode(t *testing.T) {
 	tests := []struct {
 		name  string
 		steps []ServerTestStep
@@ -81,8 +85,6 @@ func TestServer_Serve(t *testing.T) {
 		},
 	}
 
-	const address = ":11000"
-
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			ctx, stop := context.WithCancel(context.Background())
@@ -92,7 +94,7 @@ func TestServer_Serve(t *testing.T) {
 
 			server, err := di.NewServer(&config.ServerOptions{
 				Network: config.Network{
-					Address:        address,
+					Address:        ServerAddress,
 					MaxConnections: 1,
 					MaxMessageSize: 10_000,
 					IdleTimeout:    time.Second,
@@ -106,7 +108,7 @@ func TestServer_Serve(t *testing.T) {
 			}()
 
 			waitSecond(t, waitServer)
-			client, err := network.NewTCPClient(address, 10_000, time.Second)
+			client, err := network.NewTCPClient(ServerAddress, 10_000, time.Second)
 			require.NoError(t, err)
 			defer client.Close()
 
@@ -123,7 +125,96 @@ func TestServer_Serve(t *testing.T) {
 	}
 }
 
-func waitSecond(tb testing.TB, wait chan struct{}) {
+func TestServer_Serve_WithWAL(t *testing.T) {
+	waitServer := make(chan struct{})
+	waitFinish := make(chan struct{})
+
+	fs := afero.NewMemMapFs()
+
+	// Запускаем сервер первый раз
+	server := createServerWithWAL(t, fs, waitServer)
+	ctx, stop := context.WithCancel(context.Background())
+	go func() {
+		assert.NoError(t, server.Serve(ctx))
+		waitFinish <- struct{}{}
+	}()
+
+	// Отправляем команды записи на сервер БД
+	waitSecond(t, waitServer)
+	sendCommandsToServer(t, []ServerTestStep{
+		{Request: "SET key value", WantResponse: "OK"},
+		{Request: "SET foo 1", WantResponse: "OK"},
+		{Request: "SET bar 2", WantResponse: "OK"},
+		{Request: "DEL key", WantResponse: "OK"},
+		{Request: "SET baz 3", WantResponse: "OK"},
+	})
+
+	// Останавливаем сервер
+	stop()
+	waitSecond(t, waitFinish)
+
+	// Перезапускаем сервер, который должен восстановить свое состояние
+	server = createServerWithWAL(t, fs, waitServer)
+	ctx, stop = context.WithCancel(context.Background())
+	go func() {
+		assert.NoError(t, server.Serve(ctx))
+		waitFinish <- struct{}{}
+	}()
+
+	// Отправляем команды на чтение для верификации восстановленного состояния
+	waitSecond(t, waitServer)
+	sendCommandsToServer(t, []ServerTestStep{
+		{Request: "GET key", WantResponse: "$_"},
+		{Request: "GET foo", WantResponse: "1"},
+		{Request: "GET bar", WantResponse: "2"},
+		{Request: "GET baz", WantResponse: "3"},
+	})
+
+	// Останавливаем сервер
+	stop()
+	waitSecond(t, waitFinish)
+}
+
+func createServerWithWAL(tb testing.TB, fs afero.Fs, wait chan<- struct{}) *database.Server {
+	tb.Helper()
+
+	server, err := di.NewServer(&config.ServerOptions{
+		FS: fs,
+		Network: config.Network{
+			Address:        ServerAddress,
+			MaxConnections: 1,
+			MaxMessageSize: 10_000,
+			IdleTimeout:    time.Second,
+			OnServerStart:  func() { wait <- struct{}{} },
+		},
+		WAL: config.WAL{
+			Enabled:              true,
+			FlushingBatchSize:    10,
+			FlushingBatchTimeout: time.Millisecond,
+			MaxSegmentSize:       config.DefaultWALMaxSegmentSize,
+			DataDirectory:        "/wal",
+		},
+	})
+	require.NoError(tb, err)
+
+	return server
+}
+
+func sendCommandsToServer(tb testing.TB, writeSteps []ServerTestStep) {
+	tb.Helper()
+
+	client, err := network.NewTCPClient(ServerAddress, 10_000, time.Second)
+	require.NoError(tb, err)
+	defer client.Close()
+
+	for i, step := range writeSteps {
+		response, err := client.Send([]byte(step.Request))
+		require.NoError(tb, err, "step %d", i)
+		assert.Equal(tb, step.WantResponse, string(response), "step %d", i)
+	}
+}
+
+func waitSecond(tb testing.TB, wait <-chan struct{}) {
 	tb.Helper()
 	select {
 	case <-wait:
